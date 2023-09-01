7e937146cd2d41ef693c8cd903cffe7e9d9a67a0
AjayiMike <ajayimikeofficial@gmail.com>
2023-09-01 21:49:58 +0100

    fix: timer issue

--- src/hooks/useCountdown.ts
+++ src/hooks/useCountdown.ts
@@ -20,12 +20,16 @@ const useCountdown = (targetDate: number) => {
 
 const getReturnValues = (countDown: number) => {
     // calculate time left
-    const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
-    const hours = Math.floor(
-        (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    const days = Math.max(0, Math.floor(countDown / (1000 * 60 * 60 * 24)));
+    const hours = Math.max(
+        0,
+        Math.floor((countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     );
-    const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+    const minutes = Math.max(
+        0,
+        Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60))
+    );
+    const seconds = Math.max(0, Math.floor((countDown % (1000 * 60)) / 1000));
 
     return [days, hours, minutes, seconds];
 };
